edc711f779a7acd405ba0f5e2f54b5e367ca4a5e
Artem Smirnov <a.smirnov@labster.pro>
2017-09-10 04:18:44 +0300

    13. Работаем со SwipeRefreshLayout

--- app/src/main/java/com/test/packages/MainActivity.java
+++ app/src/main/java/com/test/packages/MainActivity.java
@@ -1,6 +1,7 @@
 package com.test.packages;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,17 +13,22 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     private AppManager appManager;
+    private AppsAdapter appsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        appManager = new AppManager(this);
-        List<AppInfo> installedApps = appManager.getInstalledApps();
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(onRefreshListener);
 
-        AppsAdapter appsAdapter = new AppsAdapter();
+        appManager = new AppManager(this);
+
+        appsAdapter = new AppsAdapter();
 
         RecyclerView recyclerView = findViewById(R.id.apps_rv);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -33,9 +39,22 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setAdapter(appsAdapter);
 
+        reloadApps();
+    }
 
+    private void reloadApps() {
+        List<AppInfo> installedApps = appManager.getInstalledApps();
         appsAdapter.setApps(installedApps);
         appsAdapter.notifyDataSetChanged();
+
+        swipeRefreshLayout.setRefreshing(false);
     }
+
+    private final SwipeRefreshLayout.OnRefreshListener onRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            reloadApps();
+        }
+    };
 
 }
